e01c01a7c6939cbae7edbbbe4cef135473f3a35c
Manuel <m.moracuesta@gmail.com>
2016-08-27 21:06:43 +0200

    First scene Logic for drag and drop Logic for change plane z

---
+++ Assets/Scripts/GameLogic/DragableWithMouse.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragableWithMouse : MonoBehaviour
+{
+    public LayerMask layerMask;
+    Rigidbody m_rigidBody;
+    Quaternion m_originalRotation;
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = true;
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_originalRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        
+    }
+
+    public void OnMouseUp()
+    {
+        m_rigidBody.isKinematic = false;
+    }
+
+    public void OnMouseDrag()
+    {
+        m_rigidBody.isKinematic = true;
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 1;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        Debug.DrawRay(ray.origin, ray.direction, Color.red, 3f);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000f, layerMask))
+        {
+            transform.position = hit.point;
+            transform.rotation = m_originalRotation;
+            Debug.Log("Mouse position:" + mousePos);
+        }
+        
+        //Debug.Log("Mouse position:" + mousePos);
+        //Vector3 worldPoint = Camera.main.ScreenToWorldPoint(mousePos);
+        //Debug.Log(worldPoint);
+        //Vector3 newPosition = transform.position;
+        //newPosition.x = worldPoint.x;
+        //newPosition.y = worldPoint.y;
+        //transform.position = newPosition;
+    }
+
+
+ 
+}
---
+++ Assets/Scripts/GameLogic/StructurePlane.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StructurePlane : MonoBehaviour
+{
+    const float C_SNAP_SIZE = 1.5f;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.z -= 3f;
+            transform.position = newPosition; 
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.z += 3f;
+            transform.position = newPosition; 
+
+        }
+    }
+
+}
